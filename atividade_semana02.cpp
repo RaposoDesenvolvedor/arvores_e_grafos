9e86c18304e500cfdb58febd93fbb4e7ebd345a4
João Lucas <123497461+RaposoDesenvolvedor@users.noreply.github.com>
2026-02-15 14:38:36 -0300

    Atividade Semana 2

--- atividade_semana02.cpp
+++ atividade_semana02.cpp
@@ -4,10 +4,16 @@
 
 // João Lucas Raposo Martins - 2412905
 
+void preencherVetorDecrescente(int *vetor, int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        vetor[i] = tamanho - i;
+    }
+}
+
 void preencherVetorAleatorio(int *vetor, int tamanho) {
     srand(time(NULL));
-
     for (int i = 0; i < tamanho; i++) {
+
         vetor[i] = rand() % 100;
     }
 }
@@ -15,9 +21,8 @@ void preencherVetorAleatorio(int *vetor, int tamanho) {
 void bubbleSort(int *vetor, int tamanho, int *comparacoes, int *movimentacoes) {
     for (int i = 0; i < tamanho; i++) {
         for (int j = 0; j < tamanho - 1 - i; j++) {
-            (*comparacoes)++;
-
             if (vetor[j] > vetor[j + 1]) {
+                (*comparacoes)++;
                 int temp = vetor[j];
                 vetor[j] = vetor[j + 1];
                 vetor[j + 1] = temp;
@@ -31,8 +36,8 @@ void selectionSort(int *vetor, int tamanho, int *comparacoes, int *movimentacoes
     for (int i = 0; i < tamanho; i++) {
         int menor_indice = i;
         for (int j = i + 1; j < tamanho; j++) {
-            (*comparacoes)++;
             if (vetor[j] <  vetor[menor_indice]) {
+                (*comparacoes)++;
                 menor_indice = j;
             }
         }
@@ -51,7 +56,7 @@ void executarTeste(const char *algoritmo, void (*sortFunction)(int *, int, int *
     clock_t inicio, fim;
     double tempo;
 
-    preencherVetorAleatorio(vetor, tamanho);
+    preencherVetorDecrescente(vetor, tamanho);
     inicio = clock();
     sortFunction(vetor, tamanho, &comparacoes, &movimentacoes);
     fim = clock();
@@ -76,5 +81,14 @@ int main() {
     printf("|---------------|---------|------------|--------------|----------------|\n");
     executarTeste("Selection Sort", selectionSort, 10000);
     printf("|---------------|---------|------------|--------------|----------------|\n");
+
+    printf("Pergunta: Qual dos algoritmos apresentou o melhor desempenho?\n\n");
+    printf("Neste caso, o Selection Sort apresenta desempenho superior, uma vez que o vetor não está ordenado.\n");
+    printf("O Selection Sort é mais criterioso ao realizar as movimentações, diferentemente do Bubble Sort, que realiza a movimentação em todos as posições.\n");
+    printf("Com o método do Selection Sort, ele consome menos tempo movimentando os números.\n");
+    printf("Também é possível verificar uma maior variação com o gerador de vetor aleatório.\n");
+    printf("Para utilizar, basta trocar a função 'preencherVetorDecrescente' por 'preencherVetorAleatorio' na função 'executarTeste'.\n");
+
+
     return 0;
 }
